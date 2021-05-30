d02c1d12258492c66e40dab21382aea242a359c7
guszti <agoston.kiraly@outlook.hu>
2021-05-30 17:18:06 +0200

    reorganized tests

--- src/components/running_quiz/__test__/Quiz.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Quiz from '../components/Quiz';
-import ReactDOM from 'react-dom';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Quiz questions={[{title: 'asd', answers: ['asd']}]} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
